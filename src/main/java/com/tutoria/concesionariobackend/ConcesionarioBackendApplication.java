36e11b0e3a8b6303faec38029436372bb02a83c3
ilap42 <ilap435@gmail.com>
2021-09-17 11:28:32 -0500

    Se agrega el proyecto

---
+++ src/main/java/com/tutoria/concesionariobackend/ConcesionarioBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tutoria.concesionariobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcesionarioBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcesionarioBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tutoria/concesionariobackend/ConcesionarioBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutoria.concesionariobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcesionarioBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
